c18e3e1306e98ad5ffd458309c89198302aae0ce
arik509 <sabirhossainarik34@gmail.com>
2025-11-12 21:10:34 +0600

    Added mongodb setup securely

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gagl2gk.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+async function run() {
+    try {
+      await client.connect();
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      
+    }
+  }
+  run().catch(console.dir);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
